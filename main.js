a0129aff49ee0652a7aea513bcba8419a438e35e
Guillermo Salinas <112260868+GuilleSalinas@users.noreply.github.com>
2024-03-31 21:54:58 -0300

    primer commit
    Pagina de plano software

---
+++ main.js
@@ -0,0 +1,7 @@
+let userBox = document.getElementById("userBox");
+function openinfo(){
+    userBox.classList.remove("hide-details");
+}
+function closeinfo(){
+    userBox.classList.add("hide-details");
+}
